44b6a71bbe0f3f0d6bb53aa351ba602916832a42
tsheta mbuyi <tshieta@gmail.com>
2024-10-24 13:38:34 +0200

    Added account router to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const loanRoutes = require('./routes/loanRoutes');
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
+const accountRoutes = require('./routes/accountRoutes');
+
 const app = express();
 
 const env = process.env.NODE_ENV || 'development';
@@ -12,6 +14,8 @@ dotenv.config({ path: `.env.${env}` });
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+// Use the account routes
+app.use('/account', accountRoutes);
 
 app.use('/auth', authRoutes);
 // Loan Application Routes
